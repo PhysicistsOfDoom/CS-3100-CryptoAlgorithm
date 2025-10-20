8c13d74bd23d85139393a3e5a72d5b299a30bbc9
PhysicistsOfDoom <corbin.beus@gmail.com>
2025-10-20 16:35:19 -0600

    Running Bash script

--- Scripts/run.sh
+++ Scripts/run.sh
@@ -1,1 +1,13 @@
 #!/bin/bash
+
+cd "$(dirname "$0")"
+cd ..
+
+# Start Backend
+echo "Starting backend..."
+uvicorn Backend.main:app --reload &
+
+# Start Frontend
+echo "Starting frontend..."
+cd Frontend
+python -m http.server 3000
